1c68a118baa3c2e23ca07abcfbba3f18fc3dc05c
hlf <97090576@qq.com>
2022-05-09 16:28:10 +0800

    20.go 有效括号的判断，内容包括使用切片实现栈的细节

--- leetcode/20.go
+++ leetcode/20.go
@@ -7,5 +7,28 @@ stack := []int{}
 
 */
 func isValid(s string) bool {
+	n := len(s)
+	if n%2 == 1 {
+		return false
+	}
 
+	m := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	var stack []byte
+	for _, v := range []byte(s) {
+		if m[v] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != m[v] {
+				return false
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			stack = append(stack, v)
+		}
+	}
+	return len(stack) == 0
 }
