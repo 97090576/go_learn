b563aee1776e55305ef3fb8b5f0b73aa889bc30b
hlf <97090576@qq.com>
2022-05-09 17:22:25 +0800

    542 01矩阵，注意最后值取反的时候，不能用 for _, v := range r ，v 是拷贝，赋值会丢失

--- leetcode/542.go
+++ leetcode/542.go
@@ -1,5 +1,34 @@
 package main
 
-//func updateMatrix(mat [][]int) [][]int {
-//
-//}
+func updateMatrix(mat [][]int) [][]int {
+	var q []pair
+	for i, v := range mat {
+		for j, vv := range v {
+			if vv == 0 {
+				q = append(q, pair{i, j})
+			}
+		}
+	}
+	level := 0
+	for len(q) > 0 {
+		level--
+		tmp := q
+		q = nil
+		for _, v := range tmp {
+			for _, d := range dirs {
+				i := v.x + d.x
+				j := v.y + d.y
+				if i >= 0 && i < len(mat) && j >= 0 && j < len(mat[0]) && mat[i][j] == 1 {
+					mat[i][j] = level
+					q = append(q, pair{i, j})
+				}
+			}
+		}
+	}
+	for _, r := range mat {
+		for i, v := range r {
+			r[i] = -v
+		}
+	}
+	return mat
+}
